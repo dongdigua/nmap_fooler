71b67c3030d3a0e22dd4216ae9a797c42f6c3cfb
dongdigua <dongdigua@outlook.com>
2022-04-24 18:06:48 +0800

    add time

--- fool.erl
+++ fool.erl
@@ -14,7 +14,7 @@ loop_acceptor(Socket) ->
 	{ok, Client} = gen_tcp:accept(Socket),
 	io:fwrite("~w", [Client]),
 	Recv = gen_tcp:recv(Client, 0),
-	io:fwrite("~w~n", [Recv]),
+	io:fwrite("~w~w~n", [Recv, calendar:local_time()]),
 	case Recv of
 		{ok, <<"HELP\r\n">>} -> gen_tcp:send(Client, "FLAG:{YouMuffinHead}+10points");
 		_ -> nil
