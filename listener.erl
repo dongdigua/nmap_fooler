39619b768b33baeb427fc640f655a932f261bdc0
dongdigua <dongdigua@outlook.com>
2022-06-25 05:18:21 +0800

    tab to space

--- listener.erl
+++ listener.erl
@@ -9,48 +9,48 @@
 -define(CONSOLE_COLOR_NORMAL,   "\e[0;38m").
 
 main(Args) ->
-	{Int, _} = string:to_integer(Args),
-	io:fwrite("starting...~n"),
-	start(Int).
+    {Int, _} = string:to_integer(Args),
+    io:fwrite("starting...~n"),
+    start(Int).
 
 start(Port) ->
-	{ok, Socket} =
-		gen_tcp:listen(Port, [binary, {packet, 0}, {active, false}, {reuseaddr, true}]),
-	loop_acceptor(Socket, 10).
+    {ok, Socket} =
+        gen_tcp:listen(Port, [binary, {packet, 0}, {active, false}, {reuseaddr, true}]),
+    loop_acceptor(Socket, 10).
 
 loop_acceptor(Socket, Num) ->
-	io:fwrite("~s[POOL] ~w~s~n", [?CONSOLE_COLOR_YELLOW, Num, ?CONSOLE_COLOR_NORMAL]),
-	{ok, Client} = gen_tcp:accept(Socket),
-	if
-		Num > 1 ->
-			process_flag(trap_exit, true),
-			spawn_link(fun() -> serve(Client) end),
-			receive
-				{'EXIT', _, _} ->
-					loop_acceptor(Socket, Num)
-			after 1 ->
-					loop_acceptor(Socket, Num - 1)
-			end;
+    io:fwrite("~s[POOL] ~w~s~n", [?CONSOLE_COLOR_YELLOW, Num, ?CONSOLE_COLOR_NORMAL]),
+    {ok, Client} = gen_tcp:accept(Socket),
+    if
+        Num > 1 ->
+            process_flag(trap_exit, true),
+            spawn_link(fun() -> serve(Client) end),
+            receive
+                {'EXIT', _, _} ->
+                    loop_acceptor(Socket, Num)
+            after 1 ->
+                    loop_acceptor(Socket, Num - 1)
+            end;
 
-		true ->
-			receive
-				_ ->
-					loop_acceptor(Socket, Num + 1)
-			after 0 ->
-					loop_acceptor(Socket, Num)
-			end
-	end.
+        true ->
+            receive
+                _ ->
+                    loop_acceptor(Socket, Num + 1)
+            after 0 ->
+                    loop_acceptor(Socket, Num)
+            end
+    end.
 
 serve(Client) ->
-	Recv = gen_tcp:recv(Client, 0),
-	io:fwrite("~s~w~s", [?CONSOLE_COLOR_BLUE,  inet:peername(Client), ?CONSOLE_COLOR_NORMAL]),
-	io:fwrite("~p~s~w~s~n", [Recv, ?CONSOLE_COLOR_GREEN, calendar:local_time(), ?CONSOLE_COLOR_NORMAL]),
+    Recv = gen_tcp:recv(Client, 0),
+    io:fwrite("~s~w~s", [?CONSOLE_COLOR_BLUE,  inet:peername(Client), ?CONSOLE_COLOR_NORMAL]),
+    io:fwrite("~p~s~w~s~n", [Recv, ?CONSOLE_COLOR_GREEN, calendar:local_time(), ?CONSOLE_COLOR_NORMAL]),
 
-	case Recv of
-		{ok, <<"HELP\r\n">>} ->
-			gen_tcp:send(Client, "FLAG:{YouMuffinHead}+10points\r\n"),
-			gen_tcp:close(Client);
-		_ -> gen_tcp:close(Client)
-	end.
+    case Recv of
+        {ok, <<"HELP\r\n">>} ->
+            gen_tcp:send(Client, "FLAG:{YouMuffinHead}+10points\r\n"),
+            gen_tcp:close(Client);
+        _ -> gen_tcp:close(Client)
+    end.
 
 
--- test.erl
+++ test.erl
@@ -2,23 +2,23 @@
 -export([nano_get_url/0, nano_get_url/1]).
 
 nano_get_url() ->
-	nano_get_url("www.baidu.com").   %in elixir use ''
+    nano_get_url("www.baidu.com").   %in elixir use ''
 
 nano_get_url(Host) ->
   % 1. `binary` - receives data as binaries (instead of lists)
   % 2. `{packet, line}` - receives data line by line
   % 3. `active: false` - blocks on `gen_tcp:recv/2` until data is available
   % 4. `reuseaddr: true` - allows us to reuse the address if the listener crashes
-	{ok, Socket} = gen_tcp:connect(Host, 80, [binary, {packet, 0}]),
-	ok = gen_tcp:send(Socket, "GET / HTTP/1.0\r\n\r\n"),
-	receive_data(Socket, []).
-	%like flush(), it receives data from port
+    {ok, Socket} = gen_tcp:connect(Host, 80, [binary, {packet, 0}]),
+    ok = gen_tcp:send(Socket, "GET / HTTP/1.0\r\n\r\n"),
+    receive_data(Socket, []).
+    %like flush(), it receives data from port
 
 receive_data(Socket, SoFar) ->
-	receive
-		{tcp, Socket, Bin} ->
-			receive_data(Socket, [Bin | SoFar]);
-		{tcp_closed, Socket} ->
-			list_to_binary(lists:reverse(SoFar))
-	end.
+    receive
+        {tcp, Socket, Bin} ->
+            receive_data(Socket, [Bin | SoFar]);
+        {tcp_closed, Socket} ->
+            list_to_binary(lists:reverse(SoFar))
+    end.
 
